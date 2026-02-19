605384ee070b7421c836ccf111c34d9b89da108d
faizanKhan <faizankhan46849@gmail.com>
2026-02-19 23:06:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import connectDb from "./config/db.js";
+import dotenv from "dotenv"
+import cors from "cors"
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 8003;
+
+// middlewares
+app.use(express.json());
+app.use(cors({
+   origin : "http://localhost:5173",
+   credentials : true,
+   methods : "GET,POST,PUT,DELETE",
+}))
+
+app.listen(PORT,()=> {
+    connectDb()
+    console.log(`Server started at ${PORT}`)
+})
