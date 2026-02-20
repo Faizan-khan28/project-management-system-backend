f5e471e17212e9c6a2e280151b221245c879a47e
faizanKhan <faizankhan46849@gmail.com>
2026-02-20 16:25:11 +0530

    minar change in product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 productRouter.post("/create-product",createProduct)
 productRouter.get("/get-all-product",getAllProducts)
 productRouter.get("/get-single-product/:id",getSingleProduct)
-productRouter.patch("/update-product/:id",updateProduct)
+productRouter.put("/update-product/:id",updateProduct)
 productRouter.delete("/delete-product/:id",deleteProduct)
 
 export default productRouter;
