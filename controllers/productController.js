6a3ac1dada0a915021bf2e58bc949385efaf58c6
faizanKhan <faizankhan46849@gmail.com>
2026-02-20 10:37:56 +0530

    getAllProducts controller done

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,3 +17,15 @@ export const createProduct = async (req,res) => {
         return res.status(500).json({message: `Error in createProduct ${error.message}`})
     }
 }
+
+export const getAllProducts = async (req,res) => {
+    try {
+        const product = await Product.find();
+        if(product.length === 0) {
+            return res.status(404).json({message: "No Product Found"})
+        }
+        return res.status(200).json({message:`Products fetched successfully ${product}`})
+    } catch (error) {
+        return res.status(500).json({message: `Error in getAllProducts ${error.message}`})
+    }
+}
