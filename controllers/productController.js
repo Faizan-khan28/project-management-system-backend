fb881a14722a25c729d7bd1c985df8e44090dceb
faizanKhan <faizankhan46849@gmail.com>
2026-02-20 10:51:58 +0530

    getsingleProduct controller done

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,7 +12,7 @@ export const createProduct = async (req,res) => {
         const product = await Product.create({
             name,price,category,inStock
         })
-        return res.status(201).json({message: `Product created successfully ${product}`})
+        return res.status(201).json({message: "Product created successfully",product})
     } catch (error) {
         return res.status(500).json({message: `Error in createProduct ${error.message}`})
     }
@@ -24,8 +24,21 @@ export const getAllProducts = async (req,res) => {
         if(product.length === 0) {
             return res.status(404).json({message: "No Product Found"})
         }
-        return res.status(200).json({message:`Products fetched successfully ${product}`})
+        return res.status(200).json({message:"Products fetched successfully",product})
     } catch (error) {
         return res.status(500).json({message: `Error in getAllProducts ${error.message}`})
+    }
+}
+
+export const getSingleProduct = async (req,res) => {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+        if(!product) {
+            return res.status(404).json({message: "product Not Found"})
+        }
+        return res.status(200).json({message:"Product fetched successfully",product})
+    } catch (error) {
+        return res.status(500).json({message: `Error in getSingleProduct ${error.message}`})
     }
 }
