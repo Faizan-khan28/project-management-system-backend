f6933a2652d0bd1f0fe483278d7c7c3bc57a23aa
faizanKhan <faizankhan46849@gmail.com>
2026-02-20 12:51:41 +0530

    product routes done

---
+++ routes/productRoute.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { createProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post("/create-product",createProduct)
+productRouter.get("/get-all-product",getAllProducts)
+productRouter.get("/get-single-product/:id",getSingleProduct)
+productRouter.patch("/update-product/:id",updateProduct)
+productRouter.delete("/delete-product/:id",deleteProduct)
+
+export default productRouter;
