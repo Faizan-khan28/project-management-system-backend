6f815849fc5b58deec0d062cb20d7b09c0ed0497
faizanKhan <faizankhan46849@gmail.com>
2026-02-20 21:31:04 +0530

    changing in cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8003;
 
 app.use(express.json());
 app.use(cors({
-   origin : "http://localhost:5173",
+   origin : "*",
    credentials : true,
    methods : "GET,POST,PUT,PATCH,DELETE",
 }))
